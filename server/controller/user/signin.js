7f1f002c60e1ca2d285c519cb93bc4ceb8d28466
TheJang <wkddnjs7310@naver.com>
2020-05-10 18:19:02 +0900

    server/signout.js completed

--- server/controller/user/signin.js
+++ server/controller/user/signin.js
@@ -3,6 +3,10 @@ const { users } = require('../../models');
 module.exports = {
   post: (req, res) => {
     // TODO : 유저가 로그인을 했을 때, 회원정보를 데이터베이스에서 확인하고, 회원의 id를 session에 담아주도록 구현하세요.
+    // 1) 유저의 로그인을 확인 한다.
+    // 2) 유저의 회원정보를 데이터베이스에서 확인한다.
+    // 3) 회원의 id 를 session에 담아준다.
+
     res.end();
-  }
+  },
 };
--- server/controller/user/signout.js
+++ server/controller/user/signout.js
@@ -1,6 +1,12 @@
 module.exports = {
   post: (req, res) => {
     // TODO : 유저가 로그아웃했을 때, session 정보를 없애고, '/'로 redirect할 수 있도록 구현하세요.
+    // 1) 유저의 session 정보를 없엔다.
+    // 2) res.redirect '/' 로 해준다.
+
+    // req.session 삭제
+    delete req.session;
+    res.redirect('/');
     res.end();
-  }
+  },
 };
